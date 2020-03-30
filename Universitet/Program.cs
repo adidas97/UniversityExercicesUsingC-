0f90840c1be7fc84860495c89fe9fa1be64a4118
Yavor <adidas97yg@gmail.com>
2020-03-30 14:07:38 +0300

    Add project files.

---
+++ Universitet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Universitet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
